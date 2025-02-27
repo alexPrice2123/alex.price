2d4d33a56e63788f2c8ec21ce63dc4a3387b6004
alexPrice2123 <alexander.price2123@gmail.com>
2025-02-26 21:20:18 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -184,28 +184,7 @@
 
 			});
 
-	// Footer.
-		var $footer = $('#footer');
-
-		// Copyright.
-		// This basically just moves the copyright line to the end of the *last* sibling of its current parent
-		// when the "medium" breakpoint activates, and moves it back when it deactivates.
-			$footer.find('.copyright').each(function() {
-
-				var $this = $(this),
-					$parent = $this.parent(),
-					$lastParent = $parent.parent().children().last();
-
-				breakpoints.on('<=medium', function() {
-					//$this.appendTo($lastParent);
-				});
-
-				breakpoints.on('>medium', function() {
-					//$this.appendTo($parent);
-				});
-
-			});
-
+	
 	// Main.
 		var $main = $('#main');
 
